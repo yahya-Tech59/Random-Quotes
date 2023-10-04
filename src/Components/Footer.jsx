8f73b363e5ae5049a7dcac438e9b15ecc9e958ab
yahya-cade <yahyakade790@gmail.com>
2023-10-04 12:20:36 +0300

    the end

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div id="footer">
+      <span>My Github: 👉</span>
+      <a href="https://github.com/yahya-cade/Random-Quotes/tree/main">
+        🐺yahya-cade🐺
+      </a>
+    </div>
+  );
 };
